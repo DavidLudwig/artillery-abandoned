ed2a201f8bcbe6a4089dd9205f2d31dc01c63e43
DavidLudwig <dludwig@pobox.com>
2011-11-24 22:50:18 -0500

    FixedUpdater improvements

--- Engine/FixedUpdater.js
+++ Engine/FixedUpdater.js
@@ -1,10 +1,28 @@
 
 function FixedUpdater() {
+	this._callbacks = [];
 	this._currentTime = 0;
+}
+
+FixedUpdater.prototype.AddCallback = function (interval, callback) {
+	var callbackInfo = {
+		"interval": interval,
+		"callback": callback,
+		"nextTime": interval	// TODO: set to something reflecting the current time
+	};
+	this._callbacks.push(callbackInfo);
 }
 
 FixedUpdater.prototype.AdvanceTime = function (offset) {
 	this._currentTime += offset;
+	
+	for (var i = 0; i < this._callbacks.length; i++) {
+		var callbackInfo = this._callbacks[i];
+		while (callbackInfo.nextTime <= this._currentTime) {
+			callbackInfo.callback();
+			callbackInfo.nextTime += callbackInfo.interval;
+		}
+	}
 }
 
 FixedUpdater.prototype.Time = function () {
--- Engine/Tests/Specifications/FixedUpdaterSpec.js
+++ Engine/Tests/Specifications/FixedUpdaterSpec.js
@@ -22,5 +22,83 @@ describe("FixedUpdater", function () {
 		
 		updater.AdvanceTime(50);
 		expect(updater.Time()).toEqual(90);
-	});	
+	});
+	
+	describe("Callback Invocation", function () {
+//	it("can have a callback invoked on intervals as time elapses", function () {
+		var callCount = 0;
+
+		beforeEach(function () {
+			callCount = 0;
+			updater.AddCallback(10, function () {
+				callCount++;
+			});
+		});
+		
+		it("isn't called by default", function () {
+			expect(callCount).toEqual(0);
+		});
+		
+		it("is called if time is advanced by the interval, exactly", function () {
+			updater.AdvanceTime(10);
+			expect(callCount).toEqual(1);			
+		});
+
+		it("isn't called if time is advanced by shorter than the interval", function () {
+			updater.AdvanceTime(9);
+			expect(callCount).toEqual(0);
+		});
+		
+		it("is called if time is advanced beyond the interval", function () {
+			updater.AdvanceTime(15);
+			expect(callCount).toEqual(1);
+		});
+
+		it("is called if time is advanced multiple times to reach the interval", function () {
+			updater.AdvanceTime(9);
+			updater.AdvanceTime(1);
+			expect(callCount).toEqual(1);
+		});
+		
+		it("has its callback invoked multiple times, once the interval is reached", function () {
+			updater.AdvanceTime(10);
+			expect(callCount).toEqual(1);
+			
+			updater.AdvanceTime(10);
+			expect(callCount).toEqual(2);
+			
+			updater.AdvanceTime(15);
+			expect(callCount).toEqual(3);
+			
+			updater.AdvanceTime(5);
+			expect(callCount).toEqual(4);
+		});
+		
+		it("can have its callback invoked multiple times via one time advancement", function () {
+			updater.AdvanceTime(50);
+			expect(callCount).toEqual(5);
+
+			updater.AdvanceTime(25);
+			expect(callCount).toEqual(7);
+
+			updater.AdvanceTime(25);
+			expect(callCount).toEqual(10);
+		});
+		
+		/*
+		// Call it again.
+		updater.AdvanceTime(10);
+		expect(callCount).toEqual(2);
+		
+		// Call it halfway.
+		updater.AdvanceTime(5);
+		expect(callCount).toEqual(2);
+		
+		// Call it the rest of the way.
+		updater.AdvanceTime(5);
+		expect(callCount).toEqual(3);
+		
+		// Call it multiple times.
+		*/
+	});
 });
