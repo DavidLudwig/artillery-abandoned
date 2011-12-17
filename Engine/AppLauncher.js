2ba0fc2e734a8980954eb2adbcdd91a67a49ffbb
DavidLudwig <dludwig@pobox.com>
2011-12-17 11:29:28 -0500

    removed processing; renamed ProcessingLauncher to AppLauncher

--- Engine/AppLauncher.js
+++ Engine/AppLauncher.js
@@ -1,46 +1,43 @@
 
 /*
- * Launches a Processing.js context after loading all resources.
+ * Launches a game after loading all resources.
  *
  * Example:
- *   var launcher = new ProcessingLauncher(document.getElementById('TheCanvas'), TheSketchFunction);
+ *   var launcher = new AppLauncher(document.getElementById('TheCanvas'), TheMainFunction);
  *   launcher.AddRemoteImage("Image1.png");
  *   launcher.AddRemoteImage("Image2.png");
  *   launcher.LoadRemoteImagesAndRun();
  */
-function ProcessingLauncher(canvas, mainFunction) {
+function AppLauncher(canvas, mainFunction) {
 	this.NumImagesToLoad = 0;
 	this.NumImagesLoaded = 0;
 	this.ImagesToLoad = new Array();
-	//this.sketch = new Processing.Sketch(sketchProc);
 	this.mainFunction = mainFunction;
 	this.canvas = canvas;
-	this.processingContext = null;
 }
 
-ProcessingLauncher.prototype.AddRemoteImage = function (src) {
+AppLauncher.prototype.AddRemoteImage = function (src) {
 	var launcher = this;
 	var innerImage = new Image();
 	innerImage.onload = function () {
-        //launcher.sketch.imageCache.add(src, innerImage);
         launcher._OnImageLoaded();
     }
 	this.ImagesToLoad[src] = innerImage;
 	this.NumImagesToLoad++;
 }
 
-ProcessingLauncher.prototype.GetImage = function (imageName) {
+AppLauncher.prototype.GetImage = function (imageName) {
 	return this.ImagesToLoad[imageName];
 }
 
-ProcessingLauncher.prototype._OnImageLoaded = function () {
+AppLauncher.prototype._OnImageLoaded = function () {
     this.NumImagesLoaded++;
     if (this.NumImagesLoaded == this.NumImagesToLoad) {
 		this.mainFunction();
     }
 }
 
-ProcessingLauncher.prototype.LoadRemoteImagesAndRun = function () {
+AppLauncher.prototype.LoadRemoteImagesAndRun = function () {
 	for (var src in this.ImagesToLoad) {
 	    this.ImagesToLoad[src].src = src;
 	}
--- Game/main.js
+++ Game/main.js
@@ -2,12 +2,12 @@
 var launcher;
 const LoopIntervalMS = 1000.0 / 60.0;
 
-function Main(processing) {
+function Main() {
 	InitGame();
 	setInterval(DrawGame, LoopIntervalMS);
 }
 
-launcher = new ProcessingLauncher(document.getElementById('GameCanvas'), Main);
+launcher = new AppLauncher(document.getElementById('GameCanvas'), Main);
 launcher.AddRemoteImage("Assets/Images/Background_2.png");
 launcher.AddRemoteImage("Assets/Images/Sunscape.png");
 launcher.LoadRemoteImagesAndRun();
